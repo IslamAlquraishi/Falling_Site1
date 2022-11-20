9fa0255f9f561b525d24eec3ba0a0cf478b1076d
IslamAl-Quraishi <eslamalsayed8133@gmail.com>
2022-11-20 19:30:13 +0200

    Update After Professional

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 onscroll = function () {
-  // Scroll For Background
-  head_Bg();
+  // Background For Header
+  header_bg();
 
-  // Scroll Cards
-  {
-    // For Services
-    cards();
-    // For Portfolio
-    cardFolio();
-    // For Live
-    cardLive();
-    // For Progress Span
-    skills_Span();
-  }
+  // Show Cards With Scroll
+  card_Scroll(Cards_Service, 100);
+  card_Scroll(Cards_Portfolio, 200);
+  card_Scroll(Cards_Live, 200);
 
   // Counter For Goals
-  if (window.scrollY + 100 >= SectionWork.offsetTop) {
+  if (window.scrollY + 150 >= SectionWork.offsetTop) {
     if (started == false) {
       goals.forEach((goal) => {
         countGoals(goal);
@@ -24,17 +17,17 @@ onscroll = function () {
     started = true;
   }
 
-  // Start Scroll Up
-  this.scrollY >= 2500 ? up.classList.add("show") : up.classList.remove("show");
+  // Progress Width For Span
+  skills_Span();
 
-  // Function Scroll Up
-  Up();
+  // Show Toggle Up
+  scroll();
 
   // End Window Scroll
 };
 
-// Scroll For Background
-function head_Bg() {
+// Background For Header
+function header_bg() {
   let header = document.getElementById("header");
   if (scrollY <= 125) {
     // 0%
@@ -51,34 +44,55 @@ function head_Bg() {
   }
 }
 
-// Counter For Goals
-let SectionWork = document.getElementById("work");
-let goals = document.querySelectorAll(".stat .goal");
-let started = false;
+// Open List links
+let toggle = document.getElementById("toggle");
+let bars = document.getElementById("bars");
+let xmark = document.getElementById("xmark");
+let List = document.getElementById("list");
+let container = document.querySelector(".container");
 
-// Function Counter For Goals
-function countGoals(e) {
-  let goal = e.dataset.goal;
-  let count = setInterval(() => {
-    e.textContent++;
-    if (e.textContent == goal) {
-      clearInterval(count);
-    }
-  }, 1500 / goal);
+bars.addEventListener("click", () => {
+  bars.classList.remove("rotate");
+  xmark.classList.add("rotate");
+  toggle.style.border = "2px solid #f00";
+
+  List.classList.add("show");
+});
+xmark.addEventListener("click", () => {
+  xmark.classList.remove("rotate");
+  bars.classList.add("rotate");
+  toggle.style.border = "none";
+  List.classList.remove("show");
+});
+
+// Close List Links By click AnyWay
+document.addEventListener("click", (e) => {
+  if (e.target != bars && e.target != xmark && e.target != container) {
+    toggle.style.border = "none";
+    bars.classList.add("rotate");
+    xmark.classList.remove("rotate");
+    List.classList.remove("show");
+  }
+});
+
+// Close List By Link
+function CloseBars() {
+  toggle.classList.remove("active");
+  List.classList.remove("show");
 }
 
 // Border Botton For Bar Link
 let links = document.querySelectorAll(".list .link");
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    links.forEach((l) => {
-      l.classList.remove("show");
+    links.forEach((link) => {
+      link.classList.remove("show");
     });
     link.classList.add("show");
   });
 });
 
-// The Scroll Progress Width For Span
+// Progress Width For Span
 function skills_Span() {
   let OurSkills = document.getElementById("skills");
 
@@ -106,125 +120,104 @@ function skills_Span() {
   }
 }
 
-// Scroll Window for Show Card Services
-function cards() {
-  let serviceCards = document.querySelectorAll(".services-content .card");
-  serviceCards.forEach((Card) => {
-    let CardOffSetTop = Card.offsetTop;
-    let CardOuterHeight = Card.offsetHeight;
-    let windowHeight = window.innerHeight;
-    let windowScrollTop = window.pageYOffset;
+// Counter For Goals
+let SectionWork = document.getElementById("work");
+let goals = document.querySelectorAll(".stat .goal");
+let started = false;
 
-    if (
-      windowScrollTop >
-      CardOffSetTop + 1600 + CardOuterHeight - windowHeight
-    ) {
-      Card.classList.add("show");
+// Function Counter For Goals
+function countGoals(e) {
+  let goal = e.dataset.goal;
+  let count = setInterval(() => {
+    e.textContent++;
+    if (e.textContent == goal) {
+      clearInterval(count);
     }
-    false;
-  });
+  }, 1500 / goal);
 }
 
 // Start FeedBack
-let users = document.querySelectorAll(".slide-content .user");
-let span_Num = document.querySelectorAll(".span-toggle span");
-let usersCount = users.length;
-indexNext = 1;
-let Prev = document.getElementById("prev");
-let Next = document.getElementById("next");
+{
+  // Start FeedBack
+  let users = document.querySelectorAll(".slide-content .user");
+  let span_Num = document.querySelectorAll(".span-toggle span");
+  let usersCount = users.length;
+  indexNext = 1;
+  let Prev = document.getElementById("prev");
+  let Next = document.getElementById("next");
 
-function Checker() {
-  removeActive();
-  users[indexNext - 1].classList.add("active");
-  span_Num[indexNext - 1].classList.add("active");
-
-  if (indexNext == 1) {
+  onload = () => {
     Prev.classList.add("not_allowed");
-    Prev.classList.remove("active");
-    Next.classList.add("active");
-  } else {
-    Prev.classList.remove("not_allowed");
-    Prev.classList.add("active");
+  };
+
+  function Checker() {
+    removeActive();
+    users[indexNext - 1].classList.add("active");
+    span_Num[indexNext - 1].classList.add("active");
+    if (indexNext == 1) {
+      Prev.classList.add("not_allowed");
+      Prev.classList.remove("active");
+      Next.classList.add("active");
+    } else {
+      Prev.classList.remove("not_allowed");
+      Prev.classList.add("active");
+    }
+    if (indexNext == usersCount) {
+      Next.classList.add("not_allowed");
+      Next.classList.remove("active");
+      Prev.classList.add("active");
+    } else {
+      Next.classList.remove("not_allowed");
+      Next.classList.add("active");
+    }
   }
-  if (indexNext == usersCount) {
-    Next.classList.add("not_allowed");
-    Next.classList.remove("active");
-    Prev.classList.add("active");
-  } else {
-    Next.classList.remove("not_allowed");
-    Next.classList.add("active");
+
+  // Remove All Active From Users
+  function removeActive() {
+    users.forEach((user) => {
+      user.classList.remove("active");
+    });
+    span_Num.forEach((span) => {
+      span.classList.remove("active");
+    });
   }
+
+  // Next User
+  Next.onclick = () => {
+    if (Next.classList.contains("not_allowed")) {
+      return false;
+    } else {
+      indexNext++;
+      Checker();
+    }
+  };
+
+  // Prev User
+  Prev.onclick = () => {
+    if (Prev.classList.contains("not_allowed")) {
+      return false;
+    } else {
+      indexNext--;
+      Checker();
+    }
+  };
 }
-
-// Remove All Active From Users
-function removeActive() {
-  users.forEach((user) => {
-    user.classList.remove("active");
-  });
-  span_Num.forEach((span) => {
-    span.classList.remove("active");
-  });
-}
-
-// Next User
-Next.onclick = () => {
-  if (Next.classList.contains("not_allowed")) {
-    return false;
-  } else {
-    indexNext++;
-    Checker();
-  }
-};
-
-// Prev User
-Prev.onclick = () => {
-  if (Prev.classList.contains("not_allowed")) {
-    return false;
-  } else {
-    indexNext--;
-    Checker();
-  }
-};
-
 // End FeedBack
 
-let toggle = document.getElementById("toggle");
-let List = document.getElementById("list");
-let container = document.querySelector(".container");
+// Add Class Active On Card By Scroll
+let Cards_Service = document.querySelectorAll(".services-content .card");
+let Cards_Portfolio = document.querySelectorAll(".portfolio-content .card");
+let Cards_Live = document.querySelectorAll(".live-content .card");
 
-// Open List links
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("active");
-  List.classList.toggle("show");
-});
-
-// Close List By Link
-function CloseBars() {
-  toggle.classList.remove("active");
-  List.classList.remove("show");
-}
-
-// Close List Links By click AnyWay
-document.addEventListener("click", (e) => {
-  e.stopPropagation;
-  if (e.target !== toggle && e.target !== List && e.target !== container) {
-    toggle.classList.remove("active");
-    List.classList.remove("show");
-  }
-});
-
-// Scroll Window for Show Card Portfolio
-function cardFolio() {
-  let portfolioCards = document.querySelectorAll(".portfolio-content .card");
-  portfolioCards.forEach((Card) => {
+function card_Scroll(elements, time) {
+  elements.forEach((Card) => {
     let CardOffSetTop = Card.offsetTop;
     let CardOuterHeight = Card.offsetHeight;
     let windowHeight = window.innerHeight;
     let windowScrollTop = window.pageYOffset;
-
     if (
-      windowScrollTop >
-      CardOffSetTop - 150 + CardOuterHeight - windowHeight
+      windowScrollTop + time >
+      CardOffSetTop + CardOuterHeight - windowHeight
     ) {
       Card.classList.add("show");
     }
@@ -232,32 +225,16 @@ function cardFolio() {
   });
 }
 
-// Scroll Window for Show Card Live
-function cardLive() {
-  let liveCards = document.querySelectorAll(".live-content .card");
-  liveCards.forEach((Card) => {
-    let CardOffSetTop = Card.offsetTop;
-    let CardOuterHeight = Card.offsetHeight;
-    let windowHeight = window.innerHeight;
-    let windowScrollTop = window.pageYOffset;
+// Show Toggle Up
+let scroll = () => {
+  let UP = document.getElementById("up");
+  let pos = document.documentElement.scrollTop;
+  let CalcHeigth =
+    document.body.clientHeight - document.documentElement.clientHeight;
+  let scrollProgress = Math.round((Math.floor(pos) * 100) / CalcHeigth);
 
-    if (
-      windowScrollTop >
-      CardOffSetTop - 200 + CardOuterHeight - windowHeight
-    ) {
-      Card.classList.add("show");
-    }
-    false;
+  UP.style.background = `conic-gradient(#0f0 ${scrollProgress}% , #00000030 ${scrollProgress}%)`;
+  UP.addEventListener("click", () => {
+    document.documentElement.scrollTop = "0";
   });
-}
-
-// Start Click Up
-let up = document.getElementById("up");
-function Up() {
-  up.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
-}
+};
